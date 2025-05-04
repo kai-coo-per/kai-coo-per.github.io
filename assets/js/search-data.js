8dc06bf5cdb13afc319cc5db3a2af50a8ffe201f
kai-coo-per <57119348+kai-coo-per@users.noreply.github.com>
2025-05-04 17:03:34 +0000

    Deploying to gh-pages from @ kai-coo-per/kai-coo-per.github.io@30186f17211974f9c3e36a4ca84e893220521dba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,25 +3,25 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
+  },{id: "nav-teaching",
           title: "teaching",
           description: "Courses I have contributed to or taught.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "post-a-post-with-plotly-js",
         
@@ -391,16 +391,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-my-work-using-causal-inference-to-unmask-racial-discrimination-in-traffic-enforcement-via-proxies-has-been-accepted-for-a-parallel-talk-at-the-international-computational-social-science-conference-ic2s2-2025-in-norrkoping-sweden",
+          title: 'My work “Using Causal Inference to Unmask Racial Discrimination in Traffic Enforcement via...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-my-contributed-submission-quantifying-causal-effects-via-temporal-regression-discontinuity-designs-with-time-varying-effects-has-been-accepted-for-a-15-minute-presentation-at-the-joint-statistical-meetings-2025-in-nashville-tn",
+          title: 'My contributed submission “Quantifying Causal Effects via Temporal Regression Discontinuity Designs with Time-Varying...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-my-poster-using-causal-inference-to-unmask-racial-discrimination-in-traffic-enforcement-via-proxies-has-been-accepted-for-presentation-at-acic-2025-in-detroit",
+          title: 'My poster: “Using Causal Inference to Unmask Racial Discrimination in Traffic Enforcement via...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
